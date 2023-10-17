b0df8577ba391aac105699bf69cfa557b25b9b60
choimina <choime321@gmail.com>
2023-10-17 11:08:21 +0900

    merge func 설명

--- merge_sort.py
+++ merge_sort.py
@@ -6,10 +6,28 @@ array = [random.randrange(100) for _ in range(30)]
 l_arr = len(array)
 print(array, l_arr)
 
+def merge(arr, first_left, first_right, end):  #end = inclusive
+    l, r = first_left, first_right
+    # 두 그룹에 선수가 있으면
+    while A반에 선수 있다 and B반에 선수 있다:
+        # 진 선수는 가서 줄 서 있어
+        if A반 선수가 졌어 or 비겼어:
+            A반 선수 줄 서 있어
+        else:
+            B반 선수 줄 서 있어
+    while A반에 선수 있다:
+        남은 선수추가
+    while B반에 선수 있다:
+        남은 선수추가
 def mergeSort(arr, beg, end):   # end = inclusive
-    first_rights = (beg + end + 1) // 2
-    mergeSort(arr, beg, first_rights - 1)
-    mergeSort(arr, first_rights, end)
-    merge()
+    # size = end - beg + 1
+    # if size <= 1: return
+    if beg >= end: return
+    first_right = (beg + end + 1) // 2
+    mergeSort(arr, beg, first_right - 1)
+    mergeSort(arr, first_right, end)
+    merge(arr, beg, first_right, end)
+
 
 mergeSort(array, 0, l_arr - 1)
+print(array)
